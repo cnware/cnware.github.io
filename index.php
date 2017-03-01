fafe50f06be8dee3761b136beb5aad0ccb427427
cnware <cnware@buffalo.edu>
2017-02-28 19:01:03 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$from = 'From: InterestedPerson';
+$to = 'cnware@buffalo.edu';
+$subject = $_POST['subject'];
+
+$body = "From: $name\n E-Mail: $email\n Message:\n $message";
+?>
+<?php
+if ($_POST['submit']) {
+if (mail ($to, $subject, $body, $from)) {
+echo '<p>Your message has been sent!</p>';
+} else {
+echo '<p>Something went wrong, go back and try again!</p>';
+}
+}
+?>
